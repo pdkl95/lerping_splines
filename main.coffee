4b220e174c7bd9cd7948737ae72730ab7b6f1439
Brent Sanders <git@thoughtnoise.net>
2023-01-18 11:30:58 -0800

    getting close to working

--- main.coffee
+++ main.coffee
@@ -1,37 +1,206 @@
 APP = null
 
+TAU = 2 * Math.PI
+
+class Point
+  constructor: (x, y, @color) ->
+    @order = 0
+    @radius = 5
+    @color ?= '#000'
+    @move x, y
+
+  move: (x, y) ->
+    @x = x
+    @y = y
+
+    @ix = Math.floor(@x)
+    @iy = Math.floor(@y)
+
+  position: ->
+    [@x, @y]
+
+  draw: ->
+    #console.log('draw point', @x, @y, @color)
+    ctx = APP.graph_ctx
+    ctx.beginPath()
+    ctx.fillStyle = @color
+    ctx.arc(@x, @y, @radius, 0, TAU);
+    ctx.fill()
+
+class LERP extends Point
+  constructor: (@from, @to) ->
+    @order = @from.order + 1
+
+    @radius = 5
+
+    color_fract = @order / (APP.max_lerp_order + 2)
+    color_fract *= 255
+    @color = "rgb(#{color_fract},#{color_fract},#{color_fract})"
+    console.log("lerp<#{@order}> color", @color)
+
+  interpolate: (t, a, b) ->
+    (t * a) + ((1 - t) * b)
+
+  position: (t) ->
+    [ @interpolate(t, @from.x, @to.x), @interpolate(t, @from.x, @to.x) ]
+
+  draw: (t) ->
+    p = @position(t)
+    #console.log("draw lerp<#{@order}", p)
+    ctx = APP.graph_ctx
+    ctx.strokeStyle = @color
+
+    ctx.beginPath()
+    ctx.lineWidth = 1
+    ctx.moveTo(@from.x, @from.y)
+    ctx.lineTo(@to.x, @to.y)
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.lineWidth = 2
+    ctx.arc(p[0], p[1], @radius, 0, TAU);
+    ctx.stroke()
+    
 
 class LERPingSplines
-  points: []
-
   constructor: (@context) ->
 
   init: () ->
+    console.log('Starting init()...')
+
     @running = false
 
     @content_el       = @context.getElementById('content')
 
     @graph_wrapper   = @context.getElementById('graph_wrapper')
     @graph_canvas    = @context.getElementById('graph')
-    @graph_ui_canvas = @context.getElementById('graph_ui')
+    #@graph_ui_canvas = @context.getElementById('graph_ui')
 
     @graph_ctx    = @graph_canvas.getContext('2d', alpha: true)
-    @graph_ui_ctx = @graph_ui_canvas.getContext('2d', alpha: true)
+    #graph_ui_ctx = @graph_ui_canvas.getContext('2d', alpha: true)
+
+    @graph_width  = @graph_canvas.width
+    @graph_height = @graph_canvas.height
+
+    @points = []
+    @set_max_lerp_order(3)
 
     @btn_run = $('#button_run').checkboxradio(icon: false)
-
-    #@btn_run.addEventListener 'click', @on_run
+    @btn_run.change(@on_btn_run_change)
 
     @num_points = $('#num_points').spinner
-       change: @on_num_pointa_changed
+       change: @on_num_points_changed
+       stop:   @on_num_points_changed
 
+    @tvar = $('#tvar')
+    @tslider_handle = $('#tbox_slider_handle')
+    @tslider = $('#tbox_slider').slider
+      slide: @on_tslider_slide
+#      create: ->
+#        @tslider_handle.text( @tslider.slider( "value" ) );
+
+    console.log('init() completed!')
+
+    @reset_loop()
+    @add_initial_points()
+
+  debug: (msg) ->
+    unless @debugbox?
+      @debugbox = $('#debugbox')
+      @debugbox_hdr = @debugbox.find('.hdr')
+      @debugbox_msg = @debugbox.find('.msg')
+      @debugbox.removeClass('hidden')
+
+    timestamp = new Date()
+    @debugbox_hdr.text(timestamp.toISOString())
+    @debugbox_msg.text('' + msg)
+
+  set_max_lerp_order: (n) ->
+    @max_lerp_order = n
+    for i in [0..n]
+      @points[i] ||= []
+
+  reset_loop: ->
+    @t = 0
+    @t_step = 0.02
+
+  loop_start: ->
+    @loop_running = true
+
+  loop_stop: ->
+    @loop_running = false
+
+  add_initial_points: ->
+    @add_point( 0.12 * @graph_width, 0.10 * @graph_height )
+    @add_point( 0.28 * @graph_width, 0.82 * @graph_height )
+    @add_point( 0.85 * @graph_width, 0.92 * @graph_height )
+    @add_point( 0.94 * @graph_width, 0.15 * @graph_height )
+
+    console.log('Initial points created!')
+
+  add_lerp: (from, to) ->
+    lerp = new LERP(from, to)
+    @points[lerp.order].push(lerp)
+
+  remove_lerp: (order) ->
+    #lerp =
+    @points[order].pop()
+    #lerp.destroy()
+
+  fix_num_lerps: ->
+    for i in [1..@max_lerp_order]
+      pi = i - 1
+      prev = @points[pi]
+      plen = prev.length
+      target = plen - 1
+
+      while @points[i].length < target
+        prev = @points[pi]
+        plen = prev.length
+        unless plen < 2
+          @add_lerp( prev[plen - 2], prev[plen - 1] )
+
+      # while @points[i].length > target
+      #   @remove_lerp(i)
+
+  add_point: (x, y) ->
+    p = new Point(x, y)
+    @points[0].push( p )
+    @fix_num_lerps()
+
+  remove_point: ->
+    @remove_lerp(0)
+    @fix_num_lerps()
+
+  set_num_points: (target_num) -> 
+    @add_point()    while @points.length < target_num
+    @remove_point() while @points.length > target_num
+ 
   on_num_points_changed: (event, ui) =>
+    msg = '[num_points] event: ' + event.type + ', value = ' + @num_points.val()
+    #console.log(msg)
+    @debug msg
 
-  on_run: =>
-    if @running
-      @stop()
-    else
+  on_btn_run_change: (event, ui) =>
+    checked = @btn_run.is(':checked')
+    if checked
       @start()
+    else
+      @stop()
+
+  on_tslider_slide: (event, ui) =>
+    @tslider_handle.text(ui.value)
+    #@set_t()
+
+  start: =>
+    if @running
+      # do nothing
+    else
+      @running = true
+      @schedule_first_frame()
+
+  stop: =>
+    @running = false
 
   redraw_ui: (render_bitmap_preview = true) =>
     @graph_ui_ctx.clearRect(0, 0, @graph_ui_canvas.width, @graph_ui_canvas.height)
@@ -43,19 +212,52 @@ class LERPingSplines
 
     return null
 
-  update: =>
+  update: (elapsed) =>
+    @t += @t_step
+    @t -= 1.0 while @t >= 1.0
+
+    @tvar.text(@t)
+
+  draw: ->
+    @graph_ctx.clearRect(0, 0, @graph_canvas.width, @graph_canvas.height)
+    #console.log('draw() beg')
+    console.log(@max_lerp_order, @points)
+    for order in @points
+      console.log('order', order)
+      for j in order
+        console.log('d', j)
+        j.draw(@t)
+    #console.log('draw() end')
+
+  update_callback: (timestamp) =>
     @frame_is_scheduled = false
-    @multistep()
+    elapsed = timestamp - @prev_anim_timestamp
+    if elapsed > 0
+      @prev_anim_timestamp = @anim_timestamp
+      @update(elapsed)
+      @draw()
+
     @schedule_next_frame() if @running
     return null
-
-  schedule_next_frame: () ->
+ 
+  schedule_next_frame: =>
     unless @frame_is_scheduled
       @frame_is_scheduled = true
-      window.requestAnimationFrame(@update)
+      window.requestAnimationFrame(@update_callback)
     return null
 
+  first_update_callback: (timestamp) =>
+    @anim_timestamp      = timestamp
+    @prev_anim_timestamp = timestamp
+    @frame_is_scheduled = false
+    @schedule_next_frame()
+   
+  schedule_first_frame: =>
+    @frame_is_scheduled = true
+    window.requestAnimationFrame(@first_update_callback)
+    return null
 
 $(document).ready =>
   APP = new LERPingSplines(document)
   APP.init()
+  APP.draw()
--- main.js
+++ main.js
@@ -1,52 +1,277 @@
 (function() {
-  var APP, LERPingSplines,
+  var APP, LERP, LERPingSplines, Point, TAU,
+    extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    hasProp = {}.hasOwnProperty,
     bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   APP = null;
 
-  LERPingSplines = (function() {
-    LERPingSplines.prototype.points = [];
+  TAU = 2 * Math.PI;
 
+  Point = (function() {
+    function Point(x, y, color) {
+      this.color = color;
+      this.order = 0;
+      this.radius = 5;
+      if (this.color == null) {
+        this.color = '#000';
+      }
+      this.move(x, y);
+    }
+
+    Point.prototype.move = function(x, y) {
+      this.x = x;
+      this.y = y;
+      this.ix = Math.floor(this.x);
+      return this.iy = Math.floor(this.y);
+    };
+
+    Point.prototype.position = function() {
+      return [this.x, this.y];
+    };
+
+    Point.prototype.draw = function() {
+      var ctx;
+      ctx = APP.graph_ctx;
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(this.x, this.y, this.radius, 0, TAU);
+      return ctx.fill();
+    };
+
+    return Point;
+
+  })();
+
+  LERP = (function(superClass) {
+    extend(LERP, superClass);
+
+    function LERP(from1, to1) {
+      var color_fract;
+      this.from = from1;
+      this.to = to1;
+      this.order = this.from.order + 1;
+      this.radius = 5;
+      color_fract = this.order / (APP.max_lerp_order + 2);
+      color_fract *= 255;
+      this.color = "rgb(" + color_fract + "," + color_fract + "," + color_fract + ")";
+      console.log("lerp<" + this.order + "> color", this.color);
+    }
+
+    LERP.prototype.interpolate = function(t, a, b) {
+      return (t * a) + ((1 - t) * b);
+    };
+
+    LERP.prototype.position = function(t) {
+      return [this.interpolate(t, this.from.x, this.to.x), this.interpolate(t, this.from.x, this.to.x)];
+    };
+
+    LERP.prototype.draw = function(t) {
+      var ctx, p;
+      p = this.position(t);
+      ctx = APP.graph_ctx;
+      ctx.strokeStyle = this.color;
+      ctx.beginPath();
+      ctx.lineWidth = 1;
+      ctx.moveTo(this.from.x, this.from.y);
+      ctx.lineTo(this.to.x, this.to.y);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.arc(p[0], p[1], this.radius, 0, TAU);
+      return ctx.stroke();
+    };
+
+    return LERP;
+
+  })(Point);
+
+  LERPingSplines = (function() {
     function LERPingSplines(context) {
       this.context = context;
+      this.schedule_first_frame = bind(this.schedule_first_frame, this);
+      this.first_update_callback = bind(this.first_update_callback, this);
+      this.schedule_next_frame = bind(this.schedule_next_frame, this);
+      this.update_callback = bind(this.update_callback, this);
       this.update = bind(this.update, this);
       this.redraw_ui = bind(this.redraw_ui, this);
-      this.on_run = bind(this.on_run, this);
+      this.stop = bind(this.stop, this);
+      this.start = bind(this.start, this);
+      this.on_tslider_slide = bind(this.on_tslider_slide, this);
+      this.on_btn_run_change = bind(this.on_btn_run_change, this);
       this.on_num_points_changed = bind(this.on_num_points_changed, this);
     }
 
     LERPingSplines.prototype.init = function() {
+      console.log('Starting init()...');
       this.running = false;
       this.content_el = this.context.getElementById('content');
       this.graph_wrapper = this.context.getElementById('graph_wrapper');
       this.graph_canvas = this.context.getElementById('graph');
-      this.graph_ui_canvas = this.context.getElementById('graph_ui');
       this.graph_ctx = this.graph_canvas.getContext('2d', {
         alpha: true
       });
-      this.graph_ui_ctx = this.graph_ui_canvas.getContext('2d', {
-        alpha: true
-      });
+      this.graph_width = this.graph_canvas.width;
+      this.graph_height = this.graph_canvas.height;
+      this.points = [];
+      this.set_max_lerp_order(3);
       this.btn_run = $('#button_run').checkboxradio({
         icon: false
       });
-      return this.num_points = $('#num_points').spinner({
-        change: this.on_num_pointa_changed
+      this.btn_run.change(this.on_btn_run_change);
+      this.num_points = $('#num_points').spinner({
+        change: this.on_num_points_changed,
+        stop: this.on_num_points_changed
       });
+      this.tvar = $('#tvar');
+      this.tslider_handle = $('#tbox_slider_handle');
+      this.tslider = $('#tbox_slider').slider({
+        slide: this.on_tslider_slide
+      });
+      console.log('init() completed!');
+      this.reset_loop();
+      return this.add_initial_points();
     };
 
-    LERPingSplines.prototype.on_num_points_changed = function(event, ui) {};
+    LERPingSplines.prototype.debug = function(msg) {
+      var timestamp;
+      if (this.debugbox == null) {
+        this.debugbox = $('#debugbox');
+        this.debugbox_hdr = this.debugbox.find('.hdr');
+        this.debugbox_msg = this.debugbox.find('.msg');
+        this.debugbox.removeClass('hidden');
+      }
+      timestamp = new Date();
+      this.debugbox_hdr.text(timestamp.toISOString());
+      return this.debugbox_msg.text('' + msg);
+    };
 
-    LERPingSplines.prototype.on_run = function() {
-      if (this.running) {
-        return this.stop();
-      } else {
+    LERPingSplines.prototype.set_max_lerp_order = function(n) {
+      var base, i, k, ref, results;
+      this.max_lerp_order = n;
+      results = [];
+      for (i = k = 0, ref = n; 0 <= ref ? k <= ref : k >= ref; i = 0 <= ref ? ++k : --k) {
+        results.push((base = this.points)[i] || (base[i] = []));
+      }
+      return results;
+    };
+
+    LERPingSplines.prototype.reset_loop = function() {
+      this.t = 0;
+      return this.t_step = 0.02;
+    };
+
+    LERPingSplines.prototype.loop_start = function() {
+      return this.loop_running = true;
+    };
+
+    LERPingSplines.prototype.loop_stop = function() {
+      return this.loop_running = false;
+    };
+
+    LERPingSplines.prototype.add_initial_points = function() {
+      this.add_point(0.12 * this.graph_width, 0.10 * this.graph_height);
+      this.add_point(0.28 * this.graph_width, 0.82 * this.graph_height);
+      this.add_point(0.85 * this.graph_width, 0.92 * this.graph_height);
+      this.add_point(0.94 * this.graph_width, 0.15 * this.graph_height);
+      return console.log('Initial points created!');
+    };
+
+    LERPingSplines.prototype.add_lerp = function(from, to) {
+      var lerp;
+      lerp = new LERP(from, to);
+      return this.points[lerp.order].push(lerp);
+    };
+
+    LERPingSplines.prototype.remove_lerp = function(order) {
+      return this.points[order].pop();
+    };
+
+    LERPingSplines.prototype.fix_num_lerps = function() {
+      var i, k, pi, plen, prev, ref, results, target;
+      results = [];
+      for (i = k = 1, ref = this.max_lerp_order; 1 <= ref ? k <= ref : k >= ref; i = 1 <= ref ? ++k : --k) {
+        pi = i - 1;
+        prev = this.points[pi];
+        plen = prev.length;
+        target = plen - 1;
+        results.push((function() {
+          var results1;
+          results1 = [];
+          while (this.points[i].length < target) {
+            prev = this.points[pi];
+            plen = prev.length;
+            if (!(plen < 2)) {
+              results1.push(this.add_lerp(prev[plen - 2], prev[plen - 1]));
+            } else {
+              results1.push(void 0);
+            }
+          }
+          return results1;
+        }).call(this));
+      }
+      return results;
+    };
+
+    LERPingSplines.prototype.add_point = function(x, y) {
+      var p;
+      p = new Point(x, y);
+      this.points[0].push(p);
+      return this.fix_num_lerps();
+    };
+
+    LERPingSplines.prototype.remove_point = function() {
+      this.remove_lerp(0);
+      return this.fix_num_lerps();
+    };
+
+    LERPingSplines.prototype.set_num_points = function(target_num) {
+      var results;
+      while (this.points.length < target_num) {
+        this.add_point();
+      }
+      results = [];
+      while (this.points.length > target_num) {
+        results.push(this.remove_point());
+      }
+      return results;
+    };
+
+    LERPingSplines.prototype.on_num_points_changed = function(event, ui) {
+      var msg;
+      msg = '[num_points] event: ' + event.type + ', value = ' + this.num_points.val();
+      return this.debug(msg);
+    };
+
+    LERPingSplines.prototype.on_btn_run_change = function(event, ui) {
+      var checked;
+      checked = this.btn_run.is(':checked');
+      if (checked) {
         return this.start();
+      } else {
+        return this.stop();
       }
     };
 
+    LERPingSplines.prototype.on_tslider_slide = function(event, ui) {
+      return this.tslider_handle.text(ui.value);
+    };
+
+    LERPingSplines.prototype.start = function() {
+      if (this.running) {
+
+      } else {
+        this.running = true;
+        return this.schedule_first_frame();
+      }
+    };
+
+    LERPingSplines.prototype.stop = function() {
+      return this.running = false;
+    };
+
     LERPingSplines.prototype.redraw_ui = function(render_bitmap_preview) {
-      var i, len, p, ref, ref1;
+      var k, len, p, ref, ref1;
       if (render_bitmap_preview == null) {
         render_bitmap_preview = true;
       }
@@ -55,16 +280,53 @@
         ref.draw_ui();
       }
       ref1 = this.points;
-      for (i = 0, len = ref1.length; i < len; i++) {
-        p = ref1[i];
+      for (k = 0, len = ref1.length; k < len; k++) {
+        p = ref1[k];
         p.draw_ui();
       }
       return null;
     };
 
-    LERPingSplines.prototype.update = function() {
+    LERPingSplines.prototype.update = function(elapsed) {
+      this.t += this.t_step;
+      while (this.t >= 1.0) {
+        this.t -= 1.0;
+      }
+      return this.tvar.text(this.t);
+    };
+
+    LERPingSplines.prototype.draw = function() {
+      var j, k, len, order, ref, results;
+      this.graph_ctx.clearRect(0, 0, this.graph_canvas.width, this.graph_canvas.height);
+      console.log(this.max_lerp_order, this.points);
+      ref = this.points;
+      results = [];
+      for (k = 0, len = ref.length; k < len; k++) {
+        order = ref[k];
+        console.log('order', order);
+        results.push((function() {
+          var l, len1, results1;
+          results1 = [];
+          for (l = 0, len1 = order.length; l < len1; l++) {
+            j = order[l];
+            console.log('d', j);
+            results1.push(j.draw(this.t));
+          }
+          return results1;
+        }).call(this));
+      }
+      return results;
+    };
+
+    LERPingSplines.prototype.update_callback = function(timestamp) {
+      var elapsed;
       this.frame_is_scheduled = false;
-      this.multistep();
+      elapsed = timestamp - this.prev_anim_timestamp;
+      if (elapsed > 0) {
+        this.prev_anim_timestamp = this.anim_timestamp;
+        this.update(elapsed);
+        this.draw();
+      }
       if (this.running) {
         this.schedule_next_frame();
       }
@@ -74,8 +336,21 @@
     LERPingSplines.prototype.schedule_next_frame = function() {
       if (!this.frame_is_scheduled) {
         this.frame_is_scheduled = true;
-        window.requestAnimationFrame(this.update);
+        window.requestAnimationFrame(this.update_callback);
       }
+      return null;
+    };
+
+    LERPingSplines.prototype.first_update_callback = function(timestamp) {
+      this.anim_timestamp = timestamp;
+      this.prev_anim_timestamp = timestamp;
+      this.frame_is_scheduled = false;
+      return this.schedule_next_frame();
+    };
+
+    LERPingSplines.prototype.schedule_first_frame = function() {
+      this.frame_is_scheduled = true;
+      window.requestAnimationFrame(this.first_update_callback);
       return null;
     };
 
@@ -86,7 +361,8 @@
   $(document).ready((function(_this) {
     return function() {
       APP = new LERPingSplines(document);
-      return APP.init();
+      APP.init();
+      return APP.draw();
     };
   })(this));
 
